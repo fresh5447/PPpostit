7dae50e0c3de4574d28217cb0dd3662090fbc0d4
vagrant <vagrant@rails-dev-box>
2014-06-02 22:02:55 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pppostit::Application.config.secret_key_base = 'ec2ca2a537ead004904fb9e4b1914130582d6d76a2da436cac71ba761e6b0828aa6e2af651aef1da6c663bf29bde6e1801c87ebf90519e931d54a9dc863a61d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pppostit::Application.config.session_store :cookie_store, key: '_pppostit_session'
