4652fe64891d59ffcd83bfeeb63c27342c6b69f4
Doug Walter <doug@kosmojo.com>
2014-06-02 17:20:30 -0600

    Created Post and Comment models with relationships

---
+++ db/schema.rb
@@ -0,0 +1,19 @@
+ActiveRecord::Schema.define(version: 20140602230906) do
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
