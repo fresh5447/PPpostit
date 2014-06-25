33e93264e4ba1ae497047e50dfcfc67ab169d790
Doug Walter <doug@kosmojo.com>
2014-06-25 17:11:55 -0600

    Associated users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  has_many :posts
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,39 @@
 require 'faker'
 
-rand(10..30).times do
-  p = Post.create(title: Faker::Lorem.words(rand(1..10)).join(" "), body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
-  rand(3..10).times do
-    p.comments.create(body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
-  end
+#create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
+#create posts
+50.times do
+  Post.create(
+    user: users.sample,
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+    )
+end
+posts = Post.all
+
+#create comments
+100.times do
+  Comment.create(
+    #user: users.sample, no associating yet
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+    )
 end
 
+
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
